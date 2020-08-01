6621be3b240f98ddcdef60a758530505694d0a99
cocosname <cocolvyt@163.com>
2020-08-02 00:01:08 +0800

    hw05

---
+++ hw05/hw05.py
@@ -0,0 +1,366 @@
+from scipy.constants import year
+
+
+class VendingMachine:
+    """A vending machine that vends some product for some price.
+
+    >>> v = VendingMachine('candy', 10)
+    >>> v.vend()
+    'Machine is out of stock.'
+    >>> v.add_funds(15)
+    'Machine is out of stock. Here is your $15.'
+    >>> v.restock(2)
+    'Current candy stock: 2'
+    >>> v.vend()
+    'You must add $10 more funds.'
+    >>> v.add_funds(7)
+    'Current balance: $7'
+    >>> v.vend()
+    'You must add $3 more funds.'
+    >>> v.add_funds(5)
+    'Current balance: $12'
+    >>> v.vend()
+    'Here is your candy and $2 change.'
+    >>> v.add_funds(10)
+    'Current balance: $10'
+    >>> v.vend()
+    'Here is your candy.'
+    >>> v.add_funds(15)
+    'Machine is out of stock. Here is your $15.'
+
+    >>> w = VendingMachine('soda', 2)
+    >>> w.restock(3)
+    'Current soda stock: 3'
+    >>> w.restock(3)
+    'Current soda stock: 6'
+    >>> w.add_funds(2)
+    'Current balance: $2'
+    >>> w.vend()
+    'Here is your soda.'
+    """
+    "*** YOUR CODE HERE ***"
+    def __init__(self, product, price):
+        self.product = product
+        self.price = price
+        self.product_num = 0
+        self.balance = 0
+
+    def vend(self):
+        if self.product_num == 0 and self.balance == 0:
+            return 'Machine is out of stock.'
+        elif self.product_num == 0 and self.balance > 0:
+            return 'Machine is out of stock.  Here is your ${0}.'.format(self.balance)
+        elif self.product_num > 0 and self.balance < self.price:
+            return 'You must add ${0} more funds.'.format(self.price - self.balance)
+        elif self.product_num > 0 and self.balance == self.price:
+            self.balance = 0
+            self.product_num -= 1
+            return 'Here is your {0}.'.format(self.product)
+        elif self.product_num > 0 and self.balance > self.price:
+            change = self.balance - self.price
+            self.balance = 0
+            self.product_num -= 1
+            return 'Here is your {0} and ${1} change.'.format(self.product, change)
+
+    def add_funds(self, money):
+        if self.product_num == 0:
+            return 'Machine is out of stock. Here is your ${0}.'.format(money)
+        self.balance += money
+        return 'Current balance: ${0}'.format(self.balance)
+
+    def restock(self, stock_num):
+        self.product_num += stock_num
+        return 'Current {0} stock: {1}'.format(self.product, self.product_num)
+
+
+def branches(tree):
+    return tree[1:]
+
+
+def label(tree):
+    return tree[0]
+
+
+def is_tree(tree):
+    if type(tree) != list or len(tree) < 1:
+        return False
+    for branch in branches(tree):
+        if not is_tree(branch):
+            return False
+    return True
+
+
+def Tree(label, branches=[]):
+    for branch in branches:
+        assert is_tree(branch)
+    return [label] + list(branches)
+
+
+def preorder(t):
+    """Return a list of the entries in this tree in the order that they
+    would be visited by a preorder traversal (see problem description).
+
+    >>> numbers = Tree(1, [Tree(2), Tree(3, [Tree(4), Tree(5)]), Tree(6, [Tree(7)])])
+    >>> preorder(numbers)
+    [1, 2, 3, 4, 5, 6, 7]
+    >>> preorder(Tree(2, [Tree(4, [Tree(6)])]))
+    [2, 4, 6]
+    """
+    "*** YOUR CODE HERE ***"
+    order_list = []
+    for i in range(len(t)):
+        if type(t[i]) != list:
+            order_list.append(t[i])
+        elif len(t[i]) == 1:
+            order_list.append(t[i][0])
+        else:
+            mid_list = preorder(t[i])
+            order_list += mid_list
+    return order_list
+
+
+def store_digits(n):
+    """Stores the digits of a positive number n in a linked list.
+
+    >>> s = store_digits(1)
+    >>> s
+    Link(1)
+    >>> store_digits(2345)
+    Link(2, Link(3, Link(4, Link(5))))
+    >>> store_digits(876)
+    Link(8, Link(7, Link(6)))
+    """
+    "*** YOUR CODE HERE ***"
+    if n < 10:
+        return f"Link({n})"
+    else:
+        m = 10**(len(str(n))-1)
+        first = n // m
+        last = n % m
+        return f"Link({first},{store_digits(last)})"
+
+
+## Optional Questions
+class Mint:
+    """A mint creates coins by stamping on years.
+
+    The update method sets the mint's stamp to Mint.current_year.
+
+    >>> mint = Mint()
+    >>> mint.year
+    2020
+    >>> dime = mint.create(Dime)
+    >>> dime.year
+    2020
+    >>> Mint.current_year = 2100  # Time passes
+    >>> nickel = mint.create(Nickel)
+    >>> nickel.year     # The mint has not updated its stamp yet
+    2020
+    >>> nickel.worth()  # 5 cents + (80 - 50 years)
+    35
+    >>> mint.update()   # The mint's year is updated to 2100
+    >>> Mint.current_year = 2175     # More time passes
+    >>> mint.create(Dime).worth()    # 10 cents + (75 - 50 years)
+    35
+    >>> Mint().create(Dime).worth()  # A new mint has the current year
+    10
+    >>> dime.worth()     # 10 cents + (155 - 50 years)
+    115
+    >>> Dime.cents = 20  # Upgrade all dimes!
+    >>> dime.worth()     # 20 cents + (155 - 50 years)
+    125
+
+    """
+    current_year = 2020
+
+    def __init__(self):
+        self.update()
+
+    def create(self, kind):
+        "*** YOUR CODE HERE ***"
+        return kind(self.year)
+
+    def update(self):
+        "*** YOUR CODE HERE ***"
+        self.year = Mint.current_year
+
+class Coin:
+    def __init__(self, year):
+        self.year = year
+
+    def worth(self):
+        "*** YOUR CODE HERE ***"
+        return max(Mint.current_year - self.year - 50, 0) + self.cents
+
+class Nickel(Coin):
+    cents = 5
+
+class Dime(Coin):
+    cents = 10
+
+
+def remove_all(link , value):
+    """Remove all the nodes containing value in link. Assume that the
+    first element is never removed.
+
+    >>> l1 = Link(0, Link(2, Link(2, Link(3, Link(1, Link(2, Link(3)))))))
+    >>> print(l1)
+    <0 2 2 3 1 2 3>
+    >>> remove_all(l1, 2)
+    >>> print(l1)
+    <0 3 1 3>
+    >>> remove_all(l1, 3)
+    >>> print(l1)
+    <0 1>
+    >>> remove_all(l1, 3)
+    >>> print(l1)
+    <0 1>
+    """
+    "*** YOUR CODE HERE ***"
+    if link is not Link.empty and link.rest is not Link.empty:
+        remove_all(link.rest, value)
+        if link.rest.first == value:
+            link.rest = link.rest.rest
+
+
+def deep_map(f, link):
+    """Return a Link with the same structure as link but with fn mapped over
+    its elements. If an element is an instance of a linked list, recursively
+    apply f inside that linked list as well.
+
+    >>> s = Link(1, Link(Link(2, Link(3)), Link(4)))
+    >>> print(deep_map(lambda x: x * x, s))
+    <1 <4 9> 16>
+    >>> print(s) # unchanged
+    <1 <2 3> 4>
+    >>> print(deep_map(lambda x: 2 * x, Link(s, Link(Link(Link(5))))))
+    <<2 <4 6> 8> <<10>>>
+    """
+    "*** YOUR CODE HERE ***"
+    if link is Link.empty:
+        return link
+    if type(link.first) == Link:
+        first = deep_map(f, link.first)
+    else:
+        first = f(link.first)
+    return Link(first, deep_map(f, link.rest))
+
+
+## Link Class ##
+
+class Link:
+    """A linked list.
+
+    >>> s = Link(1)
+    >>> s.first
+    1
+    >>> s.rest is Link.empty
+    True
+    >>> s = Link(2, Link(3, Link(4)))
+    >>> s.first = 5
+    >>> s.rest.first = 6
+    >>> s.rest.rest = Link.empty
+    >>> s                                    # Displays the contents of repr(s)
+    Link(5, Link(6))
+    >>> s.rest = Link(7, Link(Link(8, Link(9))))
+    >>> s
+    Link(5, Link(7, Link(Link(8, Link(9)))))
+    >>> print(s)                             # Prints str(s)
+    <5 7 <8 9>>
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest is not Link.empty:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
+
+## Tree ADT ##
+
+class Tree:
+    """
+    >>> t = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+    >>> t.label
+    3
+    >>> t.branches[0].label
+    2
+    >>> t.branches[1].is_leaf()
+    True
+    """
+    def __init__(self, label, branches=[]):
+        for b in branches:
+            assert isinstance(b, Tree)
+        self.label = label
+        self.branches = list(branches)
+
+    def is_leaf(self):
+        return not self.branches
+
+    def map(self, fn):
+        """
+        Apply a function `fn` to each node in the tree and mutate the tree.
+
+        >>> t1 = Tree(1)
+        >>> t1.map(lambda x: x + 2)
+        >>> t1.map(lambda x : x * 4)
+        >>> t1.label
+        12
+        >>> t2 = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+        >>> t2.map(lambda x: x * x)
+        >>> t2
+        Tree(9, [Tree(4, [Tree(25)]), Tree(16)])
+        """
+        self.label = fn(self.label)
+        for b in self.branches:
+            b.map(fn)
+
+    def __contains__(self, e):
+        """
+        Determine whether an element exists in the tree.
+
+        >>> t1 = Tree(1)
+        >>> 1 in t1
+        True
+        >>> 8 in t1
+        False
+        >>> t2 = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+        >>> 6 in t2
+        False
+        >>> 5 in t2
+        True
+        """
+        if self.label == e:
+            return True
+        for b in self.branches:
+            if e in b:
+                return True
+        return False
+
+    def __repr__(self):
+        if self.branches:
+            branch_str = ', ' + repr(self.branches)
+        else:
+            branch_str = ''
+        return 'Tree({0}{1})'.format(self.label, branch_str)
+
+    def __str__(self):
+        def print_tree(t, indent=0):
+            tree_str = '  ' * indent + str(t.label) + "\n"
+            for b in t.branches:
+                tree_str += print_tree(b, indent + 1)
+            return tree_str
+        return print_tree(self).rstrip()
